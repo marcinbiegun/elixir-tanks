49ca975d87c8440e99ac5cf0490c7331ca1b6a74
Marcin Biegun <marcin@mm.st>
2020-08-16 12:57:51 +0200

    Display player as a single bunny

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,38 +8,25 @@ const app = new PIXI.Application({
 });
 document.getElementById("game").appendChild(app.view);
 
-const container = new PIXI.Container();
-
-app.stage.addChild(container);
-
 // Create a new texture
 const texture = PIXI.Texture.from("images/bunny.png");
 
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
-  const bunny = new PIXI.Sprite(texture);
-  bunny.anchor.set(0.5);
-  bunny.x = (i % 5) * 40;
-  bunny.y = Math.floor(i / 5) * 40;
-  container.addChild(bunny);
-}
+const bunny = new PIXI.Sprite(texture);
+app.stage.addChild(bunny);
 
 // Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
 
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
+// Center bunny sprite in local bunny coordinates
+bunny.pivot.x = bunny.width / 2;
+bunny.pivot.y = bunny.height / 2;
 
 // Listen for animate update
 app.ticker.add((delta) => {
-  // rotate the container!
-  // use delta to create frame-independent transform
-  // container.rotation -= 0.01 * delta;
   if (document.state == undefined) {
     return;
   }
-  container.x = document.state.x;
-  container.y = document.state.y;
+  bunny.x = document.state.x;
+  bunny.y = document.state.y;
 });
